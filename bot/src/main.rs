48c12bb9d8348a41611eebd2f36608b688e6fdbb
nquidox <nquidox@gmail.com>
2021-12-22 18:34:31 +0300

    added db read

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,6 +1,8 @@
-#[warn(unused_imports)]
+// project mods
 use datab;
 use config_reader;
+
+// crates
 use serenity::{
     async_trait,
     model::{
@@ -20,6 +22,8 @@ use serenity::{
     },
     prelude::*,
 };
+use serenity::model::prelude::Connection;
+// end of import
 
 
 struct Handler;
@@ -31,16 +35,22 @@ impl EventHandler for Handler {
 
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+            let cont = datab::get_command("duck").unwrap();
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, cont).await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
     // slash commands
+    // this block creates interactions in discord chat menu
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-
-        let new = vec![("ping", "blah blah blah"), ("ring", "ding-dong!")]; //redo this with commands from database
+        let new = vec![
+            ("ping", "blah blah blah"),
+            ("ring", "ding-dong!"),
+            ("duck", "quack")
+        ]; //redo this with commands from database
 
 
         if let Interaction::ApplicationCommand(command) = interaction {
@@ -49,7 +59,10 @@ impl EventHandler for Handler {
             let mut content = command.data.name.as_str();
             for item in 0..new.len(){
                 if content == new[item].0.to_string(){
-                    content = new[item].1
+                    content = new[item].1;
+
+                    let &from_guild = command.guild_id.unwrap().as_u64(); // gets the guild_id of msg recieved
+                    println!("{}", from_guild); // prints guild id from where command was called
                 };
             }
 
@@ -70,6 +83,9 @@ impl EventHandler for Handler {
     // ready function
 
     async fn ready(&self, ctx: Context, ready: Ready) {
+        // db init
+        datab::check_existence();
+
         println!("{} is connected!", ready.user.name);
         let guild_id = GuildId(682536422116294656);
 
@@ -77,13 +93,19 @@ impl EventHandler for Handler {
         // println!("Number of guilds in the Cache: {}", guilds_num);
         //
         //
-        // let guilds = ctx.cache.guilds().await;
+        // let guilds = &ctx.cache.guilds().await;
+        // for guild in guilds{
+        //     // let tname = "test";
+        //     println!("{}", &guild);
+        //     datab::check_servers_tables(&connection, &guild.to_string());
+        // }
         // println!("Guilds in the Cache: {:?}", guilds);
 
         // let guilds2 = ctx.cache.guild(682536422116294656).await.unwrap();
         // println!("Guild info: {:?}", guilds2.name);
 
-        //redo this part for commands list from database
+
+
         let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
             commands
                 .create_application_command(|command| {
@@ -91,6 +113,9 @@ impl EventHandler for Handler {
                 })
                 .create_application_command(|command| {
                     command.name("ring").description("Ring a bell")
+                })
+                .create_application_command(|command|{
+                    command.name("duck").description("duck")
                 })
         }).await;
         println!("I now have the following guild slash commands: {:#?}", commands);
--- datab/src/lib.rs
+++ datab/src/lib.rs
@@ -1,24 +1,76 @@
-use rusqlite::{Connection,
-               params,
-               Result};
+use rusqlite::*;
 
-// inner part
-struct Server{
-    id: u64,
-    name: String
+const COMMANDS_TABLE: &str = "all_servers_commands";
+
+struct Command{
+    guild_id: u64,
+    guild_name: String,
+    command: String,
+    description: String,
+    content: String
 }
 
-fn read_db() -> Result<Connection>{
+
+fn connect() -> Result<Connection>{
     let db = Connection::open("content/global.db")
         .expect("Create a global.db sqlite3 file in content directory.");
     Ok(db)
 }
 
-// pub part
 
-// pub fn check_servers_table(){
-//     let db = read_db();
-//     db.execute("CREATE TABLE IF NOT EXISTS servers (guild_id INTEGER, guild_name)")
-// }
+pub fn check_existence(){
+    let db = connect().expect("Unable to connect to database.");
+    let sql = &*format!("CREATE TABLE IF NOT EXISTS '{}' (\
+    guild_id INTEGER,\
+    guild_name TEXT,\
+    command TEXT,\
+    description TEXT,\
+    content TEXT)", COMMANDS_TABLE);
+    db.execute(sql, []).expect("SQL query failed.");
+}
 
-//redo the whole module from sample
+// not actual, but left for future decisions
+pub fn get_command(command: &str) -> Result<String> {
+    let db = connect().expect("Unable to connect to database.");
+    let sql = &*format!("SELECT content FROM '{}' WHERE command = :command", COMMANDS_TABLE);
+    let mut stmt = db.prepare(sql)?;
+    let mut rows = stmt.query(named_params! { ":command": command })?;
+    let mut res = String::new();
+
+    while let Some(row) = rows.next()? {
+        res = row.get(0)?;
+    }
+    Ok(res)
+}
+
+pub fn commands_list() -> Result<Vec<(u64, String, String, String, String)>> {
+    let mut com_list = Vec::new();
+
+    let db = connect().expect("Unable to connect to database.");
+    let sql = &*format!("SELECT * FROM '{}'", COMMANDS_TABLE);
+    let mut stmt = db.prepare(sql)?;
+
+    // looks unnecessary complicated but it works, refactor later
+    let commands_iter = stmt.query_map([], |row| {
+        Ok(Command {
+            guild_id: row.get(0)?,
+            guild_name: row.get(1)?,
+            command: row.get(2)?,
+            description: row.get(3)?,
+            content: row.get(4)?
+        })
+    })?;
+
+    for command in commands_iter {
+        let res = (
+            command.as_ref().unwrap().guild_id.clone(),
+            command.as_ref().unwrap().guild_name.clone(),
+            command.as_ref().unwrap().command.clone(),
+            command.as_ref().unwrap().description.clone(),
+            command.as_ref().unwrap().content.clone()
+        );
+        com_list.push(res);
+    }
+
+    Ok(com_list)
+}
